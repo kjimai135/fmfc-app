9685f7a973908cbf91604d535c5a5d03b0b61304
jimin <kjimai135@gmail.com>
2026-07-08 21:10:23 +0900

    환경 변수 적용 - HTTPS 보안 개선

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vqbyatnatfotmhmpdyvj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxYnlhdG5hdGZvdG1obXBkeXZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0MjQ3MDUsImV4cCI6MjA5OTAwMDcwNX0.Bb0nMPSpqyUMHVizWcJChVSCxgARl4zgY8fJR-IQy_U'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
